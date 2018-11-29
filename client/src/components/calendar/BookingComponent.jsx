f3034bcbbc26586eb3cc29bd0ae737ed053b147a
Roy Joseph <roy.p.joseph@gmail.com>
2018-11-28 21:52:27 -0500

    fixed api route in proxy server

--- client/src/components/calendar/BookingComponent.jsx
+++ client/src/components/calendar/BookingComponent.jsx
@@ -30,7 +30,7 @@ class BookingComponent extends React.Component {
 
   componentDidMount() {
     $.ajax({
-      url: 'http://localhost:8000/api/calendar/' + this.props.propertyId,
+      url: '/api/calendar/' + this.props.propertyId,
       success: (data) => {
         this.setState({
           propertyData: data
